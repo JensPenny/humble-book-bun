6b7638c7234631e75dfe7a2a63f0fbbdb876d4e7
jens <jens.penny@gmail.com>
2025-04-24 11:26:18 +0200

    pull the git repo in the container as well, to create a clean push inside the job

--- cmd/initialize_docker_git.sh
+++ cmd/initialize_docker_git.sh
@@ -9,4 +9,5 @@ git config --global user.email "$GITHUB_EMAIL"
 git config --global --add safe.directory /app # set the docker app folder as full safe dir, since this is a shared volume from the docker context
 git config --global push.autoSetupRemote true # Automatically set up the remote tracking branch when pushing
 git remote add origin "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@${GITHUB_REPO_URL}"
+git pull
 echo "Git configuration completed successfully"
